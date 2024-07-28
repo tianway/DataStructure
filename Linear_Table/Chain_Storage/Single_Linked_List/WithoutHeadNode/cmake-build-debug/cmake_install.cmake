80972ceeb1900acfea11c46d20e044159675a30d
Tianyuan Liu <tianyuanliu111@126.com>
2024-07-28 23:33:56 +0800

    更新.gitignore文件以忽略特定的IDE文件和编译产物，并清理编译日志
    本次更新主要涉及对.gitignore文件的修改，目的是忽略一些由IDE自动生成的文件和编译过程中的临时文件。这有助于保持版本控制的干净和性能。同时，清理了cmake-build-debug目录下的.ninja_log文件，去除了旧的编译条目，只保留了最新的编译记录，以优化构建过程和提高构建效率。

--- Linear_Table/Chain_Storage/Single_Linked_List/WithoutHeadNode/cmake-build-debug/cmake_install.cmake
+++ Linear_Table/Chain_Storage/Single_Linked_List/WithoutHeadNode/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/LTY/CLionProjects/Data_Structure/Linear_Table/Chain_Storage/Single_Linked_List/WithoutHeadNode
+# Install script for directory: C:/Users/LTY/CLionProjects/DataStructure/Linear_Table/Chain_Storage/Single_Linked_List/WithoutHeadNode
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/LTY/CLionProjects/Data_Structure/Linear_Table/Chain_Storage/Single_Linked_List/WithoutHeadNode/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/LTY/CLionProjects/DataStructure/Linear_Table/Chain_Storage/Single_Linked_List/WithoutHeadNode/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
