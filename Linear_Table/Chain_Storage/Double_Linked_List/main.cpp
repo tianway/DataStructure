8823cea40a9a13bb3b25bdbc139b4b2d96fbaaec
Tianyuan Liu <tianyuanliu111@126.com>
2024-07-30 14:42:46 +0800

    实现双链表的插入和删除操作
    本次提交实现了双链表的初始化、判断空表、销毁链表、获取元素、在指定位置插入元素、在指定位置前后删除元素、以及创建链表的头尾插入方法和打印链表的方法。相关功能在main函数中进行了测试，确保链表操作的正确性。
    
    此外，更新了CMake测试文件和main.cpp文件，以适应新的链表实现。测试文件记录了测试开始和结束的时间，而main.cpp文件中添加了双链表的实现代码，并演示了如何使用这些功能。
    
    通过这些变更，代码库现在支持更丰富的数据结构操作，为后续的功能开发奠定了基础。

--- Linear_Table/Chain_Storage/Double_Linked_List/main.cpp
+++ Linear_Table/Chain_Storage/Double_Linked_List/main.cpp
@@ -1,6 +1,185 @@
-#include <iostream>
+//双链表（带头结点）
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct DNode {
+    int data;
+    struct DNode *prior, *next;
+} DNode, *DLinkList;
+
+bool InitDLinkList(DLinkList &L) {
+    L = (DNode *) malloc(sizeof(DNode));
+    if (L == NULL) {
+        return false;
+    }
+    L->next = NULL;
+    L->prior = NULL;
+    return true;
+}
+
+bool IsEmpty(DLinkList L) {
+    return L->next == NULL;
+}
+
+bool DestroyList(DLinkList &L) {
+    while (L->next != NULL) {
+        DNode *p = L->next;
+        L->next = p->next;
+        free(p);
+    }
+    free(L);
+    L = NULL;
+    return true;
+}
+
+DNode *GetElem(DLinkList L, int i) {
+    if (i < 0) {
+        return NULL;
+    }
+    DNode *p = L;
+    int j = 0;
+    while (p != NULL && j < i) {
+        p = p->next;
+        j++;
+    }
+    return p;
+}
+
+bool InsertNextNode(DNode *p, int e) {
+    if (p == NULL) {
+        return false;
+    }
+    DNode *s = (DNode *) malloc(sizeof(DNode));
+    if (s == NULL) {
+        return false;
+    }
+    s->data = e;
+    s->next = p->next;
+    if (p->next != NULL) {
+        p->next->prior = s;
+    }
+    p->next = s;
+    s->prior = p;
+    return true;
+}
+
+bool InsertPriorNode(DNode *p, int e) {
+    if (p == NULL || p->prior == NULL) {
+        return false;
+    }
+    DNode *s = (DNode *) malloc(sizeof(DNode));
+    if (s == NULL) {
+        return false;
+    }
+    s->data = e;
+    s->prior = p->prior;
+    p->prior->next = s;
+    s->next = p;
+    p->prior = s;
+    return true;
+}
+
+bool ListInsert(DLinkList &L, int i, int e) {
+    DNode *p = GetElem(L, i - 1);
+    return InsertNextNode(p, e);
+}
+
+bool ListInsert_Prior(DLinkList &L, int i, int e) {
+    DNode *p = GetElem(L, i);
+    return InsertPriorNode(p, e);
+}
+
+bool DeleteNextNode(DNode *p, int &e) {
+    if (p == NULL || p->next == NULL) {
+        return false;
+    }
+    DNode *q = p->next;
+    e = q->data;
+    p->next = q->next;
+    if (q->next != NULL) {
+        q->next->prior = p;
+    }
+    free(q);
+    return true;
+}
+
+bool DeletePriorNode(DNode *p, int &e) {
+    if (p == NULL || p->prior == NULL || p->prior->prior == NULL) {
+        return false;
+    }
+    DNode *q = p->prior;
+    e = q->data;
+    p->prior = q->prior;
+    if (q->prior != NULL) {
+        q->prior->next = p;
+    }
+    free(q);
+    return true;
+}
+
+bool ListDelete(DLinkList &L, int i, int &e) {
+    DNode *p = GetElem(L, i - 1);
+    return DeleteNextNode(p, e);
+}
+
+bool ListDelete_Prior(DLinkList &L, int i, int &e) {
+    DNode *p = GetElem(L, i + 1);
+    return DeletePriorNode(p, e);
+}
+
+DLinkList ListCreate_HeadInsert(DLinkList &L) {
+    int x;
+    InitDLinkList(L);
+    scanf("%d", &x);
+    while (x != 0) {
+        InsertNextNode(L, x);
+        scanf("%d", &x);
+    }
+    return L;
+}
+
+DLinkList ListCreate_TailInsert(DLinkList &L) {
+    int x;
+    InitDLinkList(L);
+    DNode *r = L;
+    scanf("%d", &x);
+    while (x != 0) {
+        InsertNextNode(r, x);
+        r = r->next;
+        scanf("%d", &x);
+    }
+    return L;
+}
+
+void PrintList(DLinkList L) {
+    DNode *p = L->next;
+    while (p != NULL) {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+    printf("\n");
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    DLinkList L;
+    int e;
+
+    // InitDLinkList(L);
+    // ListInsert(L, 1, 1);
+    // ListInsert(L, 2, 2);
+    // ListInsert(L, 3, 3);
+    // ListInsert_Prior(L, 2, 4);
+    // PrintList(L);
+    // ListDelete(L, 2, e);
+    // ListDelete_Prior(L, 2, e);
+    // PrintList(L);
+
+    // ListCreate_HeadInsert(L);
+    // PrintList(L);
+
+    ListCreate_TailInsert(L);
+    PrintList(L);
+
+    DestroyList(L);
     return 0;
 }
