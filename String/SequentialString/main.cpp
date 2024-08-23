0672faa88128084c62ccd982744d597a81f925d8
Tianyuan Liu <tianyuanliu111@126.com>
2024-08-23 23:41:28 +0800

    ```plaintextfeat(.idea): 新建C++项目配置与代码格式设置
    - 创建项目 SequentialString并定义 CMake 配置。- 在 .idea 目录中添加多种配置文件，设置代码格式和编辑器选项。
    - 实现主串与子串处理的顺序串基本操作。
    - 配置 CMakePythonSetting 和 VcsDirectoryMappings。
    
    此提交包括了项目从创建到基本配置的全部文件，涵盖了 C++ 代码、CMake 构建配置以及 JetBrains IDE 的项目设置。
    ```

---
+++ String/SequentialString/main.cpp
@@ -0,0 +1,59 @@
+//顺序串
+#include <stdio.h>
+
+#define MAXLEN 255
+
+typedef struct {
+    char ch[MAXLEN];
+    int length;
+} SString;
+
+int StrLength(SString S) {
+    return S.length;
+}
+
+bool ClearString(SString &S) {
+    S.length = 0;
+    return true;
+}
+
+bool SubString(SString &Sub, SString S, int pos, int len) {
+    //子串范围越界
+    if (pos + len - 1 > S.length) {
+        return false;
+    }
+    for (int i = pos; i < pos + len; i++) {
+        Sub.ch[i - pos + 1] = S.ch[i];
+    }
+    Sub.length = len;
+    return true;
+}
+
+//比较操作。若S>T，返回值>0；S=T，返回值=0；S<T，返回值<0。
+bool StrCompare(SString S, SString T) {
+    for (int i = 1; i <= S.length && i <= T.length; i++) {
+        if (S.ch[i] != T.ch[i]) {
+            return S.ch[i] - T.ch[i];
+        }
+    }
+    //扫描过的所有字符都相同，则长度长的串更大
+    return S.length - T.length;
+}
+
+//主串S中查找子串T的位置，若找到，返回值是子串T在主串S中的位置；否则返回值是0。
+int Index(SString S, SString T) {
+    int i = 1, n = S.length, m = T.length;
+    SString sub;
+    while (i <= n - m + 1) {
+        SubString(sub, S, i, m);
+        if (StrCompare(sub, T) == 0) {
+            return i;
+        }
+        i++;
+    }
+    return 0;
+}
+
+int main() {
+    return 0;
+}
